d5cfb9b96468b568cd77d0a7254a89ace5e8fd7f
Paras Garg <gargsparas11@gmail.com>
2025-07-25 09:09:29 +0530

    modified routes in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
-import {ToastContainer} from 'react-toastify'
-import React, { useState,createContext } from 'react'
-import {Route,Routes} from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
+import React, { useState, createContext } from 'react'
+import { Route, Routes } from 'react-router-dom'
 
 
 import AddCourier from './pages/AddCourier'
@@ -21,26 +21,26 @@ import StaffDetails from './pages/StaffDetails'
 import AddStaff from './pages/AddStaff'
 import DeleteStaff from './pages/DeleteStaff'
 import FeedbackForm from './pages/Feedback'
-
+import ManageStaff from './pages/ManageStaff'
 
 export const AuthContext = createContext()
 
 function App() {
-  
-  const [user,setUser] = useState(null)
+
+  const [user, setUser] = useState(null)
 
 
   return (
     <div>
-      
+
       <AuthContext.Provider value={{ user, setUser }}>
 
         <Routes>
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={<Register />} />
           <Route path='/admin/dashboard' element={<AdminDashboard />} />
-          <Route path='/admin/order-history' element={<OrderHistoryAdmin/>} />
-          <Route path='/admin/manage-hub' element={<ManageHub/>} />
+          <Route path='/admin/order-history' element={<OrderHistoryAdmin />} />
+          <Route path='/admin/manage-hub' element={<ManageHub />} />
           <Route path='/user' element={<UserDashboard />} />
           <Route path='/customer/add-courier' element={<AddCourier />} />
           {/* <Route path='/view-couriers' element={<ViewCouriers />} /> */}
@@ -50,18 +50,19 @@ function App() {
           <Route path='/customer/order-history' element={<OrderHistory />} />
           <Route path='/staff/profile' element={<StaffProfile />} />
           <Route path='/customer/profile' element={<CustomerProfile />} />
-           <Route path='/staff/staff-details' element={<StaffDetails />} />
-             <Route path='/admin/add-staff' element={<AddStaff />} />
-              <Route path='/admin/delete-staff' element={<DeleteStaff />} />
-                <Route path='/customer/feedback' element={<FeedbackForm />} />
-          
+          <Route path='/staff/staff-details' element={<StaffDetails />} />
+          <Route path='/admin/add-staff' element={<AddStaff />} />
+          <Route path='/admin/delete-staff' element={<DeleteStaff />} />
+          <Route path='/customer/feedback' element={<FeedbackForm />} />
+          <Route path='/admin/manage-staff' element={<ManageStaff />} />
+
           <Route path='/' element={<HomePage />} />
         </Routes>
-        
+
       </AuthContext.Provider>
-    <ToastContainer />
+      <ToastContainer />
     </div>
-   
+
   )
 }
 
