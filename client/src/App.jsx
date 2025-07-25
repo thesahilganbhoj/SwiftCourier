0c2e603ff76360cdcbfbe267347ae34f5bd72098
Anirudha Adekar <anirudhaadekar@gmail.com>
2025-07-25 08:31:30 +0530

    Modified routes in App.jsx and Added some new routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,6 +12,10 @@ import Login from './pages/Login'
 import Register from './pages/Register'
 import StaffProfile from './pages/StaffProfile'
 import CustomerProfile from './pages/CustomerProfile'
+import StaffDetails from './pages/StaffDetails'
+import AddStaff from './pages/AddStaff'
+import DeleteStaff from './pages/DeleteStaff'
+import FeedbackForm from './pages/Feedback'
 
 
 export const AuthContext = createContext()
@@ -31,11 +35,15 @@ function App() {
           <Route path='/register' element={<Register />} />
           <Route path='/admin/dashboard' element={<AdminDashboard />} />
           <Route path='/user' element={<UserDashboard />} />
-          <Route path='/add-courier' element={<AddCourier />} />
+          <Route path='/customer/add-courier' element={<AddCourier />} />
           <Route path='/view-couriers' element={<ViewCouriers />} />
           <Route path='/track-courier' element={<TrackCourier />} />
           <Route path='/staff/profile' element={<StaffProfile />} />
           <Route path='/customer/profile' element={<CustomerProfile />} />
+           <Route path='/staff/staff-details' element={<StaffDetails />} />
+             <Route path='/admin/add-staff' element={<AddStaff />} />
+              <Route path='/admin/delete-staff' element={<DeleteStaff />} />
+                <Route path='/customer/feedback' element={<FeedbackForm />} />
           <Route path='/' element={<Login />} />
         </Routes>
         
