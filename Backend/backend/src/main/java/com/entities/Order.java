d0b5086ccbe4caec7422f7c62f58b65bb5fdf2b1
Anirudha Adekar <anirudhaadekar@gmail.com>
2025-07-28 23:19:06 +0530

    Added Entities

---
+++ Backend/backend/src/main/java/com/entities/Order.java
@@ -0,0 +1,13 @@
+package com.courier.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+public class Order {
+
+
+  }
---
+++ Backend/backend/src/main/java/com/entities/Staff.java
@@ -0,0 +1,5 @@
+package com.courier.entities;
+
+public class Staff {
+
+}
