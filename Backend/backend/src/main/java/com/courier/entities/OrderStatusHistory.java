77d50c1e8483d88f6617a41eaa54f09a87d34967
Sahil Ganbhoj <ganbhojsahil@gmail.com>
2025-08-01 18:21:29 +0530

    Completed OrderStatusHistory Entity

--- Backend/backend/src/main/java/com/courier/entities/OrderStatusHistory.java
+++ Backend/backend/src/main/java/com/courier/entities/OrderStatusHistory.java
@@ -1,5 +1,52 @@
 package com.courier.entities;
 
-public class OrderStatusHistory {
+import java.time.LocalDateTime;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name="order_status_history")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class OrderStatusHistory {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="status_Id")
+	Integer statusId;
+	
+	@ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @Column(name = "updated_by_user_id", nullable = false)
+    private Long updatedByUserId;
+
+    @Column(name = "updated_by_role", nullable = false)
+    private String updatedByRole; // values: "ADMIN", "STAFF"
+
+    @Column(name = "status", nullable = false)
+    private String status;
+
+    @Column(name = "location")
+    private String location;
+
+    @Column(name = "timestamp", nullable = false)
+    private LocalDateTime timestamp;
+
+    @Column(name = "notes")
+    private String notes;
+    
 }
