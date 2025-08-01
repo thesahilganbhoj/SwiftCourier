41586ed60fa6d5398707cc3212674b04cc50797a
Paras Garg <gargsparas11@gmail.com>
2025-08-01 18:56:38 +0530

    Created Admin entity

--- Backend/backend/src/main/java/com/courier/entities/Admin.java
+++ Backend/backend/src/main/java/com/courier/entities/Admin.java
@@ -1,5 +1,37 @@
 package com.courier.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "admins")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Admin {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "admin_id")
+    private Long adminId;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    // Custom constructor without ID (for creating new Admins)
+    public Admin(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 }
