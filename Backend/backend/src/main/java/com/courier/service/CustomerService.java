0234ba6525fd271a96980e85836cc49dcbe3ed75
Paras Garg <gargsparas11@gmail.com>
2025-08-03 17:36:21 +0530

    Created two apis in controller for customer feedback and customer profile

--- Backend/backend/src/main/java/com/courier/service/CustomerService.java
+++ Backend/backend/src/main/java/com/courier/service/CustomerService.java
@@ -1,5 +1,9 @@
 package com.courier.service;
 
-public interface CustomerService {
+import com.courier.dto.CustomerDTO;
+import com.courier.entities.Feedback;
 
+public interface CustomerService {
+    void saveFeedback(Feedback feedback);
+    CustomerDTO getCustomerById(Long id);
 }
