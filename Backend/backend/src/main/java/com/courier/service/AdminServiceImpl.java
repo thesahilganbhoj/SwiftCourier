543ccab88036258c09c12610c0a4abde1f023d0f
Anirudha Adekar <anirudhaadekar@gmail.com>
2025-08-05 11:42:36 +0530

    AdminServiceImpl Conflict Resolution + added some endpoints in it.

--- Backend/backend/src/main/java/com/courier/service/AdminServiceImpl.java
+++ Backend/backend/src/main/java/com/courier/service/AdminServiceImpl.java
@@ -1,12 +1,18 @@
 package com.courier.service;
 
-import java.util.Optional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.courier.custom_exceptions.ResourceNotFoundException;
+import com.courier.dto.ManageStaffAdminDTO;
+import com.courier.dto.OrderAdminRespDTO;
 import com.courier.entities.Admin;
+import com.courier.entities.Order;
 import com.courier.repository.AdminRepository;
+import com.courier.repository.OrderRepository;
 
 import jakarta.transaction.Transactional;
 
@@ -16,12 +22,17 @@ public class AdminServiceImpl implements AdminService{
 
 	@Autowired
     private AdminRepository adminRepository;
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
 	@Override
 	public Admin getAdminById(Long adminId) {
 		
-		  Optional admin =adminRepository.findById(adminId);
+		  Admin admin =adminRepository.findById(adminId)
+				  .orElseThrow(() -> new ResourceNotFoundException("Invalid restuarant ID"));;
 		
-		  return (Admin) admin.get();
+		  return admin;
 	}
 	
 	
@@ -30,7 +41,34 @@ public class AdminServiceImpl implements AdminService{
 		 adminRepository.save(admin);
 		 return "Admin added successfully!!!!!!!!!!!";
 	}
+
+
+	 @Override
+	    public List<OrderAdminRespDTO> getOrdersForDashboard() {
+	        // Fetch all orders from DB
+	        List<Order> orders = orderRepository.findByStatus("PENDING");
+	        		
+
+	        // Manually map each Order entity to OrderAdminRespDTO
+	        return orders.stream()
+	                .map(this::convertToAdminRespDTO)
+	                .collect(Collectors.toList());
+	    }
+
+	    // Helper method for manual mapping
+	    private OrderAdminRespDTO convertToAdminRespDTO(Order order) {
+	        OrderAdminRespDTO dto = new OrderAdminRespDTO();
+	        dto.setOrderId(order.getOrderId());
+	        dto.setCustomerId(order.getCustomer().getCustomerId());
+	        dto.setCity(order.getDestinationCity());
+	        dto.setSenderAddress(order.getSenderAddress());
+	        dto.setReceiverAddress(order.getReceiverAddress());
+	       
+	        return dto;
+	    }
 	
-	
+	    public List<ManageStaffAdminDTO> getStaffByAdminWarehouse(Long adminId) {
+	        return adminRepository.findStaffByAdminWarehouse(adminId);
+	    }
 	
 }
