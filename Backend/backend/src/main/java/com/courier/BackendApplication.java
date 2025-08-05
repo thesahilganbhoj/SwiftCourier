19bb0cf74226bf7934c489cd96622047cedbb085
Anirudha Adekar <anirudhaadekar@gmail.com>
2025-08-06 01:32:48 +0530

    Updated pom.xml -Added Model Mapper

--- Backend/backend/src/main/java/com/courier/BackendApplication.java
+++ Backend/backend/src/main/java/com/courier/BackendApplication.java
@@ -1,7 +1,9 @@
 package com.courier;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -9,5 +11,16 @@ public class BackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
+	 @Bean
+	    public ModelMapper modelMapper() {
+	        ModelMapper modelMapper = new ModelMapper();
 
+	        // Optional: Fine-tune matching strategy
+	        modelMapper.getConfiguration()
+	                .setFieldMatchingEnabled(true)
+	                .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
+	                .setMatchingStrategy(org.modelmapper.convention.MatchingStrategies.STRICT);
+
+	        return modelMapper;
+	    }
 } 
